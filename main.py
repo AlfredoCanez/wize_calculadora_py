2929ac6d0359021a54a9788d84df25f49ff744df
AlfredoCanez <alfredo_canez@hotmail.com>
2023-01-13 11:02:43 -0600

    [Main] Se agrega estructura en main para selección y manejo del programa

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ class calculadora():
         self.numeros = [0,0]
         self.encendido = True 
 
+
     def estado(self):
         return self.encendido
+
 
     def apagar(self):
         self.encendido = False
@@ -15,52 +17,53 @@ class calculadora():
 
     def capturar_numeros(self,opcion):
 
-        if opcion != 5 and opcion != 6:
+        if opcion != 7 and opcion != 8 and opcion != 9:
             self.numeros[0] = float(input("Ingresa el primer numero: "))
             self.numeros[1] = float(input("Ingresa el segundo numero: "))
         else:
             self.numeros[0] = float(input("Ingresa el numero: "))
-            self.numeros[1] = float(input("Ingresa la potencia: "))
-
-        return self.numeros
 
 
-    def suma(self,numeros):
+    def mostrar_resultado(self,res):
+        print("Resultado: ", res, "\n")
+
+
+    def suma(self):
         return self.numeros[0] + self.numeros[1]
 
 
-    def resta(self,numeros):
+    def resta(self):
         return self.numeros[0] - self.numeros[1]    
 
 
-    def multiplicacion(self,numeros):
+    def multiplicacion(self):
         return self.numeros[0] * self.numeros[1]
 
     
-    def division(self,numeros):
-        if numeros[1] == 0:
+    def division(self):
+        if self.numeros[1] == 0:
             raise ValueError("No es posible la division entre 0")
-            
-        return self.numeros[0] / self.numeros[1]
+        else:    
+            return self.numeros[0] / self.numeros[1]
 
 
-    def raiz(self,numeros):
+    def raiz(self):
         return self.numeros[0] ** (1/self.numeros[1])
 
 
-    def potencia(self,numeros):
+    def potencia(self):
         return self.numeros[0] ** self.numeros[1]
 
 
-    def seno(self,numeros):
+    def seno(self):
         return numpy.sin(self.numeros[0])
 
 
-    def coseno(self,numeros):
+    def coseno(self):
         return numpy.cos(self.numeros[0])
 
 
-    def tangente(self,numeros):
+    def tangente(self):
         return numpy.tan(self.numeros[0])
 
 
@@ -89,12 +92,30 @@ def main():
         calc.menu()
         opcion = int(input())
 
-        if opcion != 10:
-            numeros = calc.capturar_numeros(opcion)
+        if opcion == 10:
+            calc.apagar()
+        elif opcion > 0 and opcion < 9:
+            calc.capturar_numeros(opcion)
 
-            print("El resultado es: ", calc.seno(numeros), "\n")
-        else:
-            calc.apagar()    
+            if opcion == 1:
+                calc.mostrar_resultado(calc.suma())
+            elif opcion == 2:
+                calc.mostrar_resultado(calc.resta())
+            elif opcion == 3:
+                calc.mostrar_resultado(calc.multiplicacion())
+            elif opcion == 4:
+                calc.mostrar_resultado(calc.division())
+            elif opcion == 5:
+                calc.mostrar_resultado(calc.raiz())
+            elif opcion == 6:
+                calc.mostrar_resultado(calc.potencia())
+            elif opcion == 7:
+                calc.mostrar_resultado(calc.seno())
+            elif opcion == 8:
+                calc.mostrar_resultado(calc.coseno())
+            else:
+                calc.mostrar_resultado(calc.tangente())
+                
 
 if __name__ == '__main__':
     main()
